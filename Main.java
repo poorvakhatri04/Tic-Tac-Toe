18e204b575ec05a37a93ddb0881565c3ee1e6934
Poorva khatri <poorvakhatri04@gmail.com>
2024-09-22 18:34:33 +0530

    debugged the code

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 class Main{
     public static void main(String args){
         char board[][]=new char[3][3];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++);
+        int m=board[0].length;
+        int i,j;
+        for(i=0;i<board.length;i++){
+            for(j=0;j<m;j++);
             {
                 board[i][j]=' ';
             }
@@ -30,15 +32,31 @@ class Main{
             }
         }
         printBoard(board);
+        sc.close();
     }
     public static boolean haveWon(char[][]board, char player){
-
+        for(int i=0;i<board.length;i++){
+            if(board[i][0]==player && board[i][1]==player && board[i][2]==player){
+                return true;
+            }
+        }
+        for(int j=0;j<board[0].length;j++){
+            if(board[0][j]==player && board[1][j]==player && board[2][j]==player){
+                return true;
+            }
+        }
+        if(board[0][0]==player && board[1][1]==player && board[2][2]==player)
+            return true;
+        if(board[0][2]==player && board[1][1]==player && board[2][0]==player)
+            return true;
+        return false;
     }
     public static void printBoard(char[][] board){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++);
+        int row,col;
+        for(row=0;row<board.length;row++){
+            for(col=0; col< board[row].length; col++);
             {
-                System.out.print(board[i][j]+" ");
+                System.out.print(board[row][col]+" | ");
             }
             System.out.println();
         }
